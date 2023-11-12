ee0d831c8addf2c9df1b846679cb2ff57f25016a
derek <derek@email>
2023-11-12 23:20:36 +0000

    Add shell script to run all sites in serial

---
+++ metashape-shell/run-all-sites.sh
@@ -0,0 +1,8 @@
+#python3 /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/aspen-tahoe/metashape-configs/sorensons1.yml
+python3 /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/aspen-tahoe/metashape-configs/meadows-trail.yml
+python3 /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/aspen-tahoe/metashape-configs/north-canyon-nadir.yml
+python3 /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/aspen-tahoe/metashape-configs/ophir-creek.yml
+python3 /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/aspen-tahoe/metashape-configs/red-lake-1.yml
+python3 /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/aspen-tahoe/metashape-configs/red-lake-2.yml
+python3 /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/aspen-tahoe/metashape-configs/sorensons2-picket-pk.yml
+python3 /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/aspen-tahoe/metashape-configs/weber-mill-rd.yml
